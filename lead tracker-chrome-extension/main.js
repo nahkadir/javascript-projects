1c40d513171023f1989402757a03e05c33011fab
Rida Khan <199349979+nahkadir@users.noreply.github.com>
2026-02-16 21:36:39 +0500

    render functionality

--- lead tracker-chrome-extension/main.js
+++ lead tracker-chrome-extension/main.js
@@ -1,5 +1,25 @@
-function saveLead() {
-  console.log("Button clicked!");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      "<li> <a href = ' " +
+      myLeads[i] +
+      " + ' target=_blank>" +
+      myLeads[i] +
+      "</a></li>";
+  }
+  ulEl.innerHTML = listItems;
 }
 
-// 4:45
+// 5:33
